dbf20d603e21e3447e6eb6202a2adaf686c8a6e3
Людмила <kornilova203@gmail.com>
2016-11-26 16:30:12 +0300

    corrections for Max
    added photo
    menu height was increased
    principles height was increased
    opportunities' inscriptions was moved to boarder
    statistics was apdated
    3 markers was added to map
    vawes at the bottom

--- js/map.js
+++ js/map.js
@@ -5,8 +5,8 @@ var myMap,
 
 function init () {
     myMap = new ymaps.Map('map', {
-        center: [59.957128, 30.308182],
-        zoom: 16
+        center: [59.942443, 30.334161],
+        zoom: 13
     }, {
         // При сложных перестроениях можно выставить автоматическое
         // обновление карты при изменении размеров контейнера.
@@ -16,10 +16,40 @@ function init () {
     });
     myMap.geoObjects
         .add(new ymaps.Placemark([59.957128, 30.308182], {
-            balloonContent: 'Университет ИТМО'
+            hintContent: 'Университет ИТМО. Главный корпус',
+            balloonContent: 'Университет ИТМО. Главный корпус<br />Кронверкский пр-к 49'
         }, {
-            preset: 'islands#icon',
-            iconColor: '#0095b6'
+            iconLayout: 'default#image',
+            iconImageHref: 'img/metka1.png',
+            iconImageSize: [30, 30],
+            iconImageOffset: [-10, -10]
+        }))
+        .add(new ymaps.Placemark([59.929276, 30.315396], {
+            hintContent: 'Университет ИТМО',
+            balloonContent: 'Университет ИТМО<br />Гривцова 14'
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/metka4.png',
+            iconImageSize: [30, 30],
+            iconImageOffset: [-10, -10]
+        }))
+        .add(new ymaps.Placemark([59.939728, 30.349857], {
+            hintContent: 'МИР Интерактивных Разработок',
+            balloonContent: 'МИР Интерактивных Разработок<br />Короленко 4'
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/metka4.png',
+            iconImageSize: [30, 30],
+            iconImageOffset: [-10, -10]
+        }))
+        .add(new ymaps.Placemark([59.926837, 30.337894], {
+            hintContent: 'Университет ИТМО',
+            balloonContent: 'Университет ИТМО<br />Ломоносова 9'
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/metka4.png',
+            iconImageSize: [30, 30],
+            iconImageOffset: [-10, -10]
         }))
         myMap.behaviors.disable('scrollZoom');
 }
