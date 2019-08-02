c292663156975fd1e65899db78f032f2d4487a31
A. Kotelnikov <kotelnikov@enhancelab.ru>
2019-08-02 21:30:49 +0300

    slider on main page

---
+++ js/main-slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#top-slider').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+	slidesToShow: 1,
+	centerMode: true,
+	variableWidth: true
+  });
+});
